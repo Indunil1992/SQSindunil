c03ec2cb9ce11afb4c1eef10ef1a18b5c5d16909
Indunil1992 <indunil@adroitlogic.com>
2019-07-08 18:09:43 +0530

    uuu

--- SQSindunil/lambda.js
+++ SQSindunil/lambda.js
@@ -2,38 +2,33 @@ let AWS = require('aws-sdk');
 let SL_AWS = require('slappforge-sdk-aws');
 const sqs = new SL_AWS.SQS(AWS);
 
+
 exports.handler = function (event, context, callback) {
 
-    sqs.receiveMessage({
-        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/test-queue.fifo`,
-        AttributeNames: ['SenderId'],
-        MaxNumberOfMessages: '20',
-        VisibilityTimeout: '50',
-        WaitTimeSeconds: '30',
-        MessageAttributeNames: ['f']
-    }).promise()
-        .then(receivedMsgData => {
-            if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
-                let receivedMessages = receivedMsgData.Messages;
-                receivedMessages.forEach(message => {
-                    // your logic to access each message through out the loop. Each message is available under variable message 
-                    // within this block
-                    console.log("Success");
-                    console.log(data);
-                });
-            } else {
-                // No messages to process
-                console.log("No messages");
-                console.log(data);
-            }
-        })
-        .catch(err => {
-            // error handling goes here
-            console.log("error");
-            console.log(data);
-        });
+    sqs.receiveAndDeleteMessages({
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/Hiru1T`,
+        AttributeNames: ['All'],
+        MaxNumberOfMessages: '1',
+        VisibilityTimeout: '30',
+        WaitTimeSeconds: '0'
+    }, function (receivedMessages) {
 
+ console.log("receivedMessages");
+                 console.log( receivedMessages );
+        // implement received message filtering logic here and return filtered set of messages which 
+        // are allowed to delete in the next step
+        return receivedMessages;
+    }, function (deleteSuccessData) {
 
+ console.log("deleteSuccessData");
+                 console.log( deleteSuccessData );
+        // implement delete success state here
+    }, function (error) {
+
+ console.log("error");
+                 console.log( error );
+        // implement error handling logic here
+    });
 
 
     callback(null, { "message": "Successfully executed GET" });
