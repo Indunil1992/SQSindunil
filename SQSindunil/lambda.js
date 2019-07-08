7c291ef3c8c6c37c3b74140fdae547e4f4a34052
Indunil1992 <indunil@adroitlogic.com>
2019-07-08 17:47:49 +0530

    m

--- SQSindunil/lambda.js
+++ SQSindunil/lambda.js
@@ -5,11 +5,12 @@ const sqs = new SL_AWS.SQS(AWS);
 exports.handler = function (event, context, callback) {
 
     sqs.receiveMessage({
-        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/udith-test-queue`,
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/test-queue.fifo`,
         AttributeNames: ['SenderId'],
         MaxNumberOfMessages: '20',
         VisibilityTimeout: '50',
-        WaitTimeSeconds: '30'
+        WaitTimeSeconds: '30',
+        MessageAttributeNames: ['f']
     }).promise()
         .then(receivedMsgData => {
             if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
