4b11aee5028fa60584de7304de12a90b276b0458
Indunil1992 <indunil@adroitlogic.com>
2019-07-09 10:48:34 +0530

    12345

--- SQSindunil/lambda.js
+++ SQSindunil/lambda.js
@@ -2,27 +2,30 @@ let AWS = require('aws-sdk');
 let SL_AWS = require('slappforge-sdk-aws');
 const sqs = new SL_AWS.SQS(AWS);
 
-exports.handler = function (event, context, callback) {
 
-    sqs.sendMessage({
-        MessageBody: 'io',
+exports.handler = function (event, context, callback) {
+    sqs.receiveAndDeleteMessages({
         QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/Hiru1T`,
-        DelaySeconds: '0',
-        MessageAttributes: {
-            "aa": {
-                "DataType": "Number",
-                "StringValue": "12"
-            }
-        }
-    }, function (data) {
-        console.log("Success");
-        console.log(data);
-        // your logic (logging etc) to handle successful message delivery, should be here
+        AttributeNames: ['All'],
+        MaxNumberOfMessages: '10',
+        VisibilityTimeout: '30',
+        WaitTimeSeconds: '10'
+    }, function (receivedMessages) {
+        // implement received message filtering logic here and return filtered set of messages which 
+        // are allowed to delete in the next step
+         console.log("receivedMessages");
+                 console.log( receivedMessages );
+        return receivedMessages;
+    }, function (deleteSuccessData) {
+         console.log("deleteSuccessData");
+                 console.log( deleteSuccessData );
+        // implement delete success state here
     }, function (error) {
-        console.log("not success");
-        console.log(data);
-        // your logic (logging etc) to handle failures, should be here
+         console.log("No messages");
+                 console.log( error );
+        // implement error handling logic here
     });
+
 
 
 
