78214cceaea74afe70616bec5af44d2fb6eea126
Indunil1992 <indunil@adroitlogic.com>
2019-07-09 10:04:49 +0530

    1111

--- SQSindunil/lambda.js
+++ SQSindunil/lambda.js
@@ -10,20 +10,25 @@ exports.handler = function (event, context, callback) {
         MessageBody: 'test1',
         QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/sam1`,
         DelaySeconds: '0',
-        MessageAttributes: {}
+        MessageAttributes: {
+            "1": {
+                "DataType": "Number",
+                "StringValue": "123"
+            }
+        }
     }, function (data) {
-          console.log("Success");
-                 console.log( data );
+        console.log("Success");
+        console.log(data);
 
         // your logic (logging etc) to handle successful message delivery, should be here
     }, function (error) {
         console.log("not success");
-                 console.log( error );
+        console.log(error);
 
 
         // your logic (logging etc) to handle failures, should be here
     });
 
 
-    callback(null, { "message": "Successfully executed GET" });
+    callback(null, { "message": "Successfully executed new put" });
 }
