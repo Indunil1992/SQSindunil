a4c27a740fc7b192f2788db90cedf12ed2969044
Indunil1992 <indunil@adroitlogic.com>
2019-07-09 12:23:48 +0530

    x

--- SQSindunil/lambda.js
+++ SQSindunil/lambda.js
@@ -2,32 +2,29 @@ let AWS = require('aws-sdk');
 let SL_AWS = require('slappforge-sdk-aws');
 const sqs = new SL_AWS.SQS(AWS);
 
-
 exports.handler = function (event, context, callback) {
-    sqs.receiveAndDeleteMessages({
+
+    sqs.receiveMessage({
         QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/samq`,
         AttributeNames: ['All'],
-        MaxNumberOfMessages: '10',
+        MaxNumberOfMessages: '1',
         VisibilityTimeout: '30',
-        WaitTimeSeconds: '10',
-        MessageAttributeNames: ['a', 'b', 'c', 'd']
-    }, function (receivedMessages) {
-        // implement received message filtering logic here and return filtered set of messages which 
-        // are allowed to delete in the next step
-        console.log("receivedMessages");
-        console.log(receivedMessages);
-        return receivedMessages;
-    }, function (deleteSuccessData) {
-        console.log("deleteSuccessData");
-        console.log(deleteSuccessData);
-        // implement delete success state here
-    }, function (error) {
-        console.log("No messages");
-        console.log(error);
-        // implement error handling logic here
-    });
-
-
+        WaitTimeSeconds: '0'
+    }).promise()
+        .then(receivedMsgData => {
+            if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
+                let receivedMessages = receivedMsgData.Messages;
+                receivedMessages.forEach(message => {
+                    // your logic to access each message through out the loop. Each message is available under variable message 
+                    // within this block
+                });
+            } else {
+                // No messages to process
+            }
+        })
+        .catch(err => {
+            // error handling goes here
+        });
 
 
     callback(null, { "message": "Successfully executed get and remove" });
