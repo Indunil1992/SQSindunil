435d6504c812b131be4503676618270005b9fd01
Indunil1992 <indunil@adroitlogic.com>
2019-07-09 11:36:34 +0530

    4

--- SQSindunil/lambda.js
+++ SQSindunil/lambda.js
@@ -5,24 +5,25 @@ const sqs = new SL_AWS.SQS(AWS);
 
 exports.handler = function (event, context, callback) {
     sqs.receiveAndDeleteMessages({
-        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/Hiru1T`,
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/check.fifo`,
         AttributeNames: ['All'],
         MaxNumberOfMessages: '10',
         VisibilityTimeout: '30',
-        WaitTimeSeconds: '10'
+        WaitTimeSeconds: '10',
+        MessageAttributeNames: ['1', '2', '3', '4']
     }, function (receivedMessages) {
         // implement received message filtering logic here and return filtered set of messages which 
         // are allowed to delete in the next step
-         console.log("receivedMessages");
-                 console.log( receivedMessages );
+        console.log("receivedMessages");
+        console.log(receivedMessages);
         return receivedMessages;
     }, function (deleteSuccessData) {
-         console.log("deleteSuccessData");
-                 console.log( deleteSuccessData );
+        console.log("deleteSuccessData");
+        console.log(deleteSuccessData);
         // implement delete success state here
     }, function (error) {
-         console.log("No messages");
-                 console.log( error );
+        console.log("No messages");
+        console.log(error);
         // implement error handling logic here
     });
 
