5e32200bddf37676644c93f2fcd3d507cd514524
Indunil1992 <indunil@adroitlogic.com>
2019-07-11 12:13:42 +0530

    o

--- SQSindunil/lambda.js
+++ SQSindunil/lambda.js
@@ -2,36 +2,30 @@ let AWS = require('aws-sdk');
 let SL_AWS = require('slappforge-sdk-aws');
 const sqs = new SL_AWS.SQS(AWS);
 
-exports.handler = function (event, context, callback) {
-    sqs.sendMessage({
-        MessageBody: 'indu123',
-        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/Hiru1T`,
-        DelaySeconds: '0',
-        MessageAttributes: {
-            "1": {
-                "DataType": "String",
-                "StringValue": "abc"
-            },
-            "2": {
-                "DataType": "Number",
-                "StringValue": "123"
-            },
-            "3": {
-                "DataType": "Binary",
-                "BinaryValue": "0"
-            }
-        }
-    }, function (data) {
-        console.log("Success data");
-                 console.log( data );
-        // your logic (logging etc) to handle successful message delivery, should be here
-    }, function (error) {
-        console.log("not Success");
+sqs.receiveAndDeleteMessages({
+    QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/sam`,
+    AttributeNames: ['All'],
+    MaxNumberOfMessages: '10',
+    VisibilityTimeout: '30',
+    WaitTimeSeconds: '0'
+}, function (receivedMessages) {
+    console.log("received Messages");
+                 console.log( receivedMessages );
+    // implement received message filtering logic here and return filtered set of messages which 
+    // are allowed to delete in the next step
+    return receivedMessages;
+}, function (deleteSuccessData) {
+    console.log("delete SuccessData");
+                 console.log( deleteSuccessData );
+    // implement delete success state here
+}, function (error) {
+    console.log("not Success");
                  console.log( error );
-        // your logic (logging etc) to handle failures, should be here
-    });
+    // implement error handling logic here
+});
+
+exports.handler = function (event, context, callback) {
 
 
-
-    callback(null, { "message": "Successfully executed new" });
+    callback(null, { "message": "Successfully executed test6" });
 }
