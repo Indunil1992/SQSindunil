a6c1a800a2d0669697c37f185c4ea466b51f2369
Indunil1992 <indunil@adroitlogic.com>
2019-07-11 10:19:19 +0530

    d

--- SQSindunil/lambda.js
+++ SQSindunil/lambda.js
@@ -5,27 +5,35 @@ const sqs = new SL_AWS.SQS(AWS);
 exports.handler = function (event, context, callback) {
 
     sqs.receiveMessage({
-        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/sample1`,
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/induniltst1.fifo`,
         AttributeNames: ['All'],
-        MaxNumberOfMessages: '100',
+        MaxNumberOfMessages: '10',
         VisibilityTimeout: '30',
-        WaitTimeSeconds: '0'
+        WaitTimeSeconds: '0',
+        MessageAttributeNames: ['a', 's', 'd']
     }).promise()
         .then(receivedMsgData => {
             if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
                 let receivedMessages = receivedMsgData.Messages;
                 receivedMessages.forEach(message => {
+                    console.log("receivedMsgData");
+                    console.log(receivedMsgData);
                     // your logic to access each message through out the loop. Each message is available under variable message 
                     // within this block
                 });
             } else {
+                console.log("Success");
+                //console.log( data );
                 // No messages to process
             }
         })
         .catch(err => {
+            console.log("not Success");
+            console.log(err);
             // error handling goes here
         });
 
 
-    callback(null, { "message": "Successfully executed get and remove" });
+
+    callback(null, { "message": "Successfully executed fifo" });
 }
