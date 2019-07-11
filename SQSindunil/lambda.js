620d64dd213f2b38c9b9e762f5a2bff8c610dbb7
Indunil1992 <indunil@adroitlogic.com>
2019-07-11 11:29:03 +0530

    v

--- SQSindunil/lambda.js
+++ SQSindunil/lambda.js
@@ -3,20 +3,30 @@ let SL_AWS = require('slappforge-sdk-aws');
 const sqs = new SL_AWS.SQS(AWS);
 
 exports.handler = function (event, context, callback) {
-
     sqs.sendMessage({
-        MessageBody: '321',
-        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/indu.fifo`,
+        MessageBody: 'indu123',
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/Hiru1T`,
         DelaySeconds: '0',
-        MessageDeduplicationId: '123',
-        MessageGroupId: '12345',
-        MessageAttributes: {}
+        MessageAttributes: {
+            "1": {
+                "DataType": "String",
+                "StringValue": "abc"
+            },
+            "2": {
+                "DataType": "Number",
+                "StringValue": "123"
+            },
+            "3": {
+                "DataType": "Binary",
+                "BinaryValue": "0"
+            }
+        }
     }, function (data) {
-console.log("Success");
+        console.log("Success data");
                  console.log( data );
         // your logic (logging etc) to handle successful message delivery, should be here
     }, function (error) {
-console.log("not Success");
+        console.log("not Success");
                  console.log( error );
         // your logic (logging etc) to handle failures, should be here
     });
