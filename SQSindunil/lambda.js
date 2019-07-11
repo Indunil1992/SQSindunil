6e55d0f8183cbd262ff97a6ca711e505da9abd22
Indunil1992 <indunil@adroitlogic.com>
2019-07-11 10:48:25 +0530

    123

--- SQSindunil/lambda.js
+++ SQSindunil/lambda.js
@@ -4,36 +4,24 @@ const sqs = new SL_AWS.SQS(AWS);
 
 exports.handler = function (event, context, callback) {
 
-    sqs.receiveMessage({
-        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/induniltst1.fifo`,
-        AttributeNames: ['All'],
-        MaxNumberOfMessages: '10',
-        VisibilityTimeout: '30',
-        WaitTimeSeconds: '0',
-        MessageAttributeNames: ['a', 's', 'd']
-    }).promise()
-        .then(receivedMsgData => {
-            if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
-                let receivedMessages = receivedMsgData.Messages;
-                receivedMessages.forEach(message => {
-                    console.log("receivedMsgData");
-                    console.log(receivedMsgData);
-                    // your logic to access each message through out the loop. Each message is available under variable message 
-                    // within this block
-                });
-            } else {
-                console.log("Success");
-                //console.log( data );
-                // No messages to process
-            }
-        })
-        .catch(err => {
-            console.log("not Success");
-            console.log(err);
-            // error handling goes here
-        });
+    sqs.sendMessage({
+        MessageBody: '321',
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/indu.fifo`,
+        DelaySeconds: '0',
+        MessageDeduplicationId: '123',
+        MessageGroupId: '12345',
+        MessageAttributes: {}
+    }, function (data) {
+console.log("Success");
+                 console.log( data );
+        // your logic (logging etc) to handle successful message delivery, should be here
+    }, function (error) {
+console.log("not Success");
+                 console.log( error );
+        // your logic (logging etc) to handle failures, should be here
+    });
 
 
 
-    callback(null, { "message": "Successfully executed fifo" });
+    callback(null, { "message": "Successfully executed new" });
 }
